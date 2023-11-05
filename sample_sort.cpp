a73589632bb3b574a10ecbbccfdf0cc86cef964f
Emily R Wax <emily.wax@login3.cluster>
2023-11-04 22:50:30 -0500

    integrated check_sort. Seems to work. Needs further testing and cleaning up.

--- sample_sort.cpp
+++ sample_sort.cpp
@@ -103,6 +103,48 @@ void fillArray(int* values, int block_size, int NUM_VALS, int sort_type){
     }
 }
 
+void check_sort(vector<int> thread_values_array, int size){
+    int num_threads;
+    int thread_id;
+    MPI_Comm_size(MPI_COMM_WORLD,&num_threads);
+    MPI_Comm_rank(MPI_COMM_WORLD, &thread_id);
+
+    // each thread is going to check itself to see if it's sorted
+    for(int i = 0; i < size-1; i++){
+        if (thread_values_array[i] > thread_values_array[i+1]){
+            cout << "ERROR NOT SORTED\n" << endl;
+        }
+    }
+
+    // thread sends its [0] to id-1
+    if (thread_id > 0)
+        MPI_Send(&thread_values_array[0], 1, MPI_INT, thread_id-1, 0, MPI_COMM_WORLD);
+
+    // thread sends its [-1] to id+1
+    // check if index is correct 
+    if (thread_id < num_threads-1)
+        MPI_Send(&thread_values_array[thread_values_array.size() - 1], 1, MPI_INT, thread_id+1, 0, MPI_COMM_WORLD);
+
+    // thread receives id-1's [-1] and checks to see if it's < its [0]
+    int above, below;
+    if (thread_id > 0){
+        MPI_Recv(&below, 1, MPI_INT, thread_id - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        if (below > thread_values_array[0]){
+            cout << "ERROR NOT SORTED\n" << endl;
+        }
+    }
+
+    // thread receives id+1's [0] and checks to see if it's > its [-1]
+    if (thread_id < num_threads-1){
+        MPI_Recv(&above, 1, MPI_INT, thread_id + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        if (above < thread_values_array.back()){
+            cout << "ERROR NOT SORTED\n" << endl;
+        }
+    }
+
+    cout << "WOOHOO SORT CHECK DONE" << endl;
+}
+
 
 int main(int argc, char* argv[]){
     // Command Line Arguments: size, processes
@@ -122,9 +164,7 @@ int main(int argc, char* argv[]){
     int* proc_values_array = (int*) malloc (block_size * sizeof(int));
 
     // Data Generation
-    fillArray(proc_values_array, block_size, NUM_VALS, REVERSE_SORTED);
-    
-    cout << "Process " << proc_id << " has generated data\n" << endl;
+    fillArray(proc_values_array, block_size, NUM_VALS, PERTURBED);
 
     // Have each process sort locally (STL qsort) - optional, probably will not
 
@@ -138,8 +178,6 @@ int main(int argc, char* argv[]){
         local_splitter[i] = proc_values_array[NUM_VALS/(num_procs * num_procs) * (i+1)];
     }
 
-    cout << "Process " << proc_id << " has chosen local splitters\n" << endl;
-
     // Send local splitters back to root process
     global_splitter = (int *)malloc( sizeof(int) * num_procs * (num_procs - 1));
     MPI_Gather( local_splitter, num_procs - 1, MPI_INT, global_splitter, num_procs - 1, MPI_INT, 0, MPI_COMM_WORLD);
@@ -147,7 +185,6 @@ int main(int argc, char* argv[]){
     // Root: run sort on splitters array
     if(proc_id == 0)
     {
-
         qsort( (char *) global_splitter, num_procs * (num_procs - 1), sizeof(int), intcompare);
 
         // Choose p-1 Global splitters (evenly separated)
@@ -156,10 +193,10 @@ int main(int argc, char* argv[]){
             local_splitter[i] = global_splitter[(num_procs - 1) * (i + 1)];
             
         }
-        for(int i = 0; i < (num_procs - 1); i++)
-        {
-            cout << "global splitter: " << local_splitter[i] << endl;
-        }
+        // for(int i = 0; i < (num_procs - 1); i++)
+        // {
+        //     cout << "global splitter: " << local_splitter[i] << endl;
+        // }
     }
 
     // Broadcast Global Splitters to all other processes
@@ -244,11 +281,6 @@ int main(int argc, char* argv[]){
         }
     }
 
-    if( proc_id == 0 )
-    {
-
-    }
-
     // add own into bucket
     for(int i = 0; i < (buckets[proc_id]).size(); i++)
     {
@@ -259,12 +291,7 @@ int main(int argc, char* argv[]){
     sort(finalBucket.begin(), finalBucket.end());
 
     // Check if actually sorted
-    if(proc_id == 0)
-    {
-        cout << "final bucket size " << finalBucket.size() << endl;
-        for(int i = 0; i < finalBucket.size(); i++)
-            cout << finalBucket[i] << endl;
-    }
+    check_sort(finalBucket, finalBucket.size());
 
     MPI_Finalize();
 
