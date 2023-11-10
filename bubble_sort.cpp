101eddbb47157e08e76619eea4173ad9d7fc30e8
Roee Belkin <roeebelkin@login2.cluster>
2023-11-10 09:23:40 -0600

    added caliper code and file to bubble sort

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -15,6 +15,12 @@
 #include <adiak.hpp>
 
 
+
+const char* comm_small = "comm_small";
+const char* comp_small = "comp_small";
+const char* comp_medium = "comp_medium";
+
+
 using namespace std;
 
 
@@ -173,47 +179,67 @@ void bubble_sort(int* thread_values_array, int NUM_VALS){
     for (int k = 0; k < num_threads; k++){
         if (step){
             // printArrayAll(thread_values_array, block_size);
+            CALI_MARK_BEGIN(comp_small); 
             sequential_bubble(thread_values_array, 0, block_size);
+            CALI_MARK_END(comp_small); 
             // printArrayAll(thread_values_array, block_size);
             if (thread_id % 2 == 0){
                 // recieve block end_index to end_index + block size, from thread_id 1 greater than self
+                CALI_MARK_BEGIN(comm_small);
                 MPI_Recv(&thread_values_array[block_size], block_size, MPI_INT, thread_id + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                CALI_MARK_END(comm_small);
                 // printArrayAll(thread_values_array, block_size);
+                CALI_MARK_BEGIN(comp_medium);
                 sequential_bubble(thread_values_array, 0,  2 * block_size);
+                CALI_MARK_END(comp_medium);
                 // printArrayAll(thread_values_array, block_size);
                 // send second half of list (end_index to end_index + block size) to thread_id 1 greater than self
+                CALI_MARK_BEGIN(comm_small);
                 MPI_Send(&thread_values_array[block_size], block_size, MPI_INT, thread_id+1, 0, MPI_COMM_WORLD);
+                CALI_MARK_END(comm_small);
             }
             else{
                 
                 //send block start_index to end index to thread_id 1 less than self
+                CALI_MARK_BEGIN(comm_small);
                 MPI_Send(thread_values_array, block_size, MPI_INT, thread_id-1, 0, MPI_COMM_WORLD);
                 //recieve new block start_index to end index from thread_id 1 less than self
                 MPI_Recv(thread_values_array, block_size, MPI_INT, thread_id - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                CALI_MARK_END(comm_small);
             }
             step = !step;
         }
         else{
             //printArrayAll(thread_values_array, block_size);
+            CALI_MARK_BEGIN(comp_small);
             sequential_bubble(thread_values_array, 0, block_size);
+            CALI_MARK_END(comp_small);
             //printArrayAll(thread_values_array, block_size);
             if ((thread_id % 2 == 1) && (thread_id != num_threads -1)){ // not the first or last thread because those would cause the program to hang
                 // recieve block end_index to end_index + block size, from thread_id 1 greater than self
                 //printArrayAll(thread_values_array, block_size);
+                CALI_MARK_BEGIN(comm_small);
                 MPI_Recv(&thread_values_array[block_size], block_size, MPI_INT, thread_id + 1, 0, MPI_COMM_WORLD, &statusRecv);
+                CALI_MARK_END(comm_small);
                 //cout << "thread id in the top loop is: " << thread_id <<" source is: " << statusRecv.MPI_SOURCE << endl;
                 //printArrayAll(thread_values_array, block_size);
+                CALI_MARK_BEGIN(comp_medium);
                 sequential_bubble(thread_values_array, 0, 2 * block_size);
+                CALI_MARK_END(comp_medium);
                 //printArrayAll(thread_values_array, block_size);
                 // send second half of list (end_index to end_index + block size) to thread_id 1 greater than self
+                CALI_MARK_BEGIN(comm_small);
                 MPI_Send(&thread_values_array[block_size], block_size, MPI_INT, thread_id+1, 0, MPI_COMM_WORLD);
+                CALI_MARK_END(comm_small); 
             }
             else if ((thread_id % 2 == 0) && (thread_id != 0)){
                 //cout << "thread id in the bottom loop: " << thread_id << endl;
                 //send block start_index to end index to thread_id 1 less than self
+                CALI_MARK_BEGIN(comm_small);
                 MPI_Send(thread_values_array, block_size, MPI_INT, thread_id-1, 0, MPI_COMM_WORLD);
                 //recieve new block start_index to end index from thread_id 1 less than self
                 MPI_Recv(thread_values_array, block_size, MPI_INT, thread_id - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                CALI_MARK_END(comm_small); 
             }
             step = !step;
         }
@@ -269,27 +295,66 @@ void check_sort(int* thread_values_array, int block_size){
 int main(int argc, char* argv[]){
     int NUM_VALS = atoi(argv[1]);
     int num_threads;
+    int data_to_sort_type = PERTURBED; 
     // nullptr won't matter to worker threads
     
     int thread_id;
+
+    const char* whole_sort = "whole_sort";
+    const char* data_creation = "data_creation";
+    const char* correctness_check = "correctness_check";
+    const char* print_time = "print_time";
     
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD,&num_threads);
     MPI_Comm_rank(MPI_COMM_WORLD, &thread_id);
 
     int block_size = NUM_VALS / num_threads;
+
+    cali::ConfigManager mgr;
+    mgr.start();
+
     int* thread_values_array = (int*) malloc (block_size * 2 * sizeof(int));
 
     //cout << "Size is: " << num_threads << " Block Size is: " << block_size << endl;
 
-    fillArray(thread_values_array, block_size, NUM_VALS, PERTURBED);
+    CALI_MARK_BEGIN(data_creation);
+    fillArray(thread_values_array, block_size, NUM_VALS, data_to_sort_type);
+    CALI_MARK_END(data_creation);
 
+    CALI_MARK_BEGIN(whole_sort);
     bubble_sort(thread_values_array, NUM_VALS);
+    CALI_MARK_END(whole_sort);
 
 
+    CALI_MARK_BEGIN(print_time);
     printArrayTogether(thread_values_array, block_size);
+    CALI_MARK_END(print_time);
 
-    check_sort(thread_values_array, block_size); 
+    CALI_MARK_BEGIN(correctness_check);
+    check_sort(thread_values_array, block_size);
+    CALI_MARK_END(correctness_check); 
+
+    if (thread_id == 0){
+        adiak::init(NULL);
+        adiak::launchdate();    // launch date of the job
+        adiak::libraries();     // Libraries used
+        adiak::cmdline();       // Command line used to launch the job
+        adiak::clustername();   // Name of the cluster
+        adiak::value("Algorithm", "Bubble Sort"); // The name of the algorithm you are using (e.g., "MergeSort", "BitonicSort")
+        adiak::value("ProgrammingModel", "MPI"); // e.g., "MPI", "CUDA", "MPIwithCUDA"
+        adiak::value("Datatype", "int"); // The datatype of input elements (e.g., double, int, float)
+        adiak::value("SizeOfDatatype", sizeof(int)); // sizeof(datatype) of input elements in bytes (e.g., 1, 2, 4)
+        adiak::value("InputSize", NUM_VALS); // The number of elements in input dataset (1000)
+        adiak::value("InputType", data_to_sort_type); // For sorting, this would be "Sorted", "ReverseSorted", "Random", "1%perturbed"
+        adiak::value("num_procs", num_threads); // The number of processors (MPI ranks)
+        adiak::value("num_threads", num_threads); // The number of CUDA or OpenMP threads
+        adiak::value("group_num", 14); // The number of your group (integer, e.g., 1, 10)
+        adiak::value("implementation_source", "online + handwritten"); // Where you got the source code of your algorithm; choices: ("Online", "AI", "Handwritten").
+    }
+
+    mgr.stop();
+    mgr.flush();
 
     MPI_Finalize();
 }
